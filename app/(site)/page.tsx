57bf9df6588d69e3bb978dfb2f296a1569bc2643
EdsonGomesJr <edyz86@hotmail.com>
2023-07-10 12:20:39 -0300

    1h48

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,10 +8,20 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-3xl font-semibold text-white">Welcome back</h1>
           <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-            <ListItem />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+      <div className="mb-7 mt-2 px-6">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-semibold text-white">Newest songs</h1>
+        </div>
+        <div>List of Songs!</div>
+      </div>
     </div>
   )
 }
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,7 +1,8 @@
 'use client'
 
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-
+import { FaPlay } from 'react-icons/fa'
 interface ListItemProps {
   image: string
   name: string
@@ -14,9 +15,17 @@ export function ListItem({ href, image, name }: ListItemProps) {
     router.push(href)
   }
   return (
-    <button className="group relative flex items-center gap-x-4 overflow-hidden rounded-md bg-neutral-100/20 pr-4 transition">
-      <div className="relative min-h-[64px] min-w-[64px]"></div>
-      ListItem
+    <button
+      onClick={onClick}
+      className="group relative flex items-center gap-x-4 overflow-hidden rounded-md bg-neutral-100/20 pr-4 transition hover:bg-neutral-100/20"
+    >
+      <div className="relative min-h-[64px] min-w-[64px]">
+        <Image className="object-cover" fill src={image} alt="Image" />
+      </div>
+      <p className="truncate py-5 font-medium">{name}</p>
+      <div className=" absolute right-5 flex items-center justify-center rounded-full bg-green-500 p-4 opacity-0 drop-shadow-md transition hover:scale-110 group-hover:opacity-100">
+        <FaPlay className="text-black" />
+      </div>
     </button>
   )
 }
